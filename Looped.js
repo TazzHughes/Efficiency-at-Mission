13ce3482f77000ef9e9a121e2f320a1ffd56c634
Tazz Hughes <105616501+TazzHughes@users.noreply.github.com>
2022-09-21 19:01:48 -0700

    Add files via upload

---
+++ Looped.js
@@ -0,0 +1,9 @@
+function myAlert() {
+    //for loop will run/countdown 10 times
+    for(count =10; count > -1; count--) {
+    //alerts will show the actual countdown/numbers
+        alert("count=" +count);
+    }
+}
+//calling the function
+myAlert();
